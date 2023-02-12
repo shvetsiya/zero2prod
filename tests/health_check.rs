cf921bb828e66a66848120d91ebc56c6a6a26503
Alex Shvets <shvetsiya@gmail.com>
2023-02-12 10:29:41 -0800

    added formater to the code

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,20 +6,20 @@ async fn health_check_works() {
     let client = reqwest::Client::new();
 
     let response = client
-        .get(&format!("{}/health_check", address))
+        .get(&format!("{address}/health_check"))
         .send()
         .await
         .expect("Failed to execute request.");
-    
+
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port") ;
-    let port = listener.local_addr().unwrap().port() ;
-    let server = run(listener).expect("Failed to bind address") ;
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
 
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+    tokio::spawn(server);
+    format!("http://127.0.0.1:{port}")
 }
